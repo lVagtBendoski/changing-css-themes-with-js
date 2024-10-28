1d65792b97337996a28fbede0d2b10fd5bd45e91
lVagtBendoski <157419244+lVagtBendoski@users.noreply.github.com>
2024-10-28 10:29:00 -0400

    Update index.js

--- changing themes with js/index.js
+++ changing themes with js/index.js
@@ -1,22 +1,12 @@
 const textContainer = document.getElementById("intro text")
 
 initiateFall = () =>{
-    if(textContainer.classList.contains("default")){
-        textContainer.classList.remove("default");
-    }else if(textContainer.classList.contains("spring")){
-        textContainer.classList.remove("spring");
-    }else if(textContainer.classList.contains("fall")){
-        textContainer.classList.remove("fall")
-    }
+    textContainer.classList.remove("default");
+    textContainer.classList.remove("spring");
     textContainer.classList.add("fall");
 }
 initiateSpring = () =>{
-    if(textContainer.classList.contains("default")){
-        textContainer.classList.remove("default");
-    }else if(textContainer.classList.contains("spring")){
-        textContainer.classList.remove("spring");
-    }else if(textContainer.classList.contains("fall")){
-        textContainer.classList.remove("fall")
-    }
+    textContainer.classList.remove("default");
+    textContainer.classList.remove("fall");
     textContainer.classList.add("spring");
 }
